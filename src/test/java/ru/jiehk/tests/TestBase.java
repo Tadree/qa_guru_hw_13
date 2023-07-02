cebfe8e21d3d77911565888d2fa683429e866709
Elena Kosiakova <Elen.Nikitina6@x5.ru>
2023-07-02 18:56:02 +0200

    remove Configuration.holdBrowserOpen

--- src/test/java/ru/jiehk/tests/TestBase.java
+++ src/test/java/ru/jiehk/tests/TestBase.java
@@ -21,7 +21,6 @@ public class TestBase {
         Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
-        Configuration.holdBrowserOpen = true;
         Configuration.remote = System.getProperty("remoteDriverUrl","https://user1:1234@selenoid.autotests.cloud/wd/hub/");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
