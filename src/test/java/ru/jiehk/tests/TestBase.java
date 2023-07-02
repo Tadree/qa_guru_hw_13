f9ee6b7131cf7773424e91d0d7afdd134d4f2b16
Elena Kosiakova <Elen.Nikitina6@x5.ru>
2023-07-02 17:52:05 +0200

    fix build.gradle

--- src/test/java/ru/jiehk/tests/TestBase.java
+++ src/test/java/ru/jiehk/tests/TestBase.java
@@ -18,11 +18,11 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.baseUrl = System.getProperty("baseUrl");
+        Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
         Configuration.holdBrowserOpen = true;
-        Configuration.remote = System.getProperty("remoteDriverUrl");
+        Configuration.remote = System.getProperty("remoteDriverUrl","https://user1:1234@selenoid.autotests.cloud/wd/hub/");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
